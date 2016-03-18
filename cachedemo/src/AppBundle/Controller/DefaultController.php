2d3b308f103e2ca606e251c6227919e63a496362
Julien Bouffard <bouffard.jul@gmail.com>
2016-03-18 10:34:42 +0100

    test ETAG

--- cachedemo/src/AppBundle/Controller/DefaultController.php
+++ cachedemo/src/AppBundle/Controller/DefaultController.php
@@ -14,10 +14,40 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $response = new Response();
-        $response->setMaxAge(25);
+        $response = $this->render(
+            'default/index.html.twig', 
+            [
+                'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            ]
+        );
+        $response->setETag(md5($response->getContent()));
+        $response->isNotModified($request);
+        $response->setMaxAge(10);
+        $response->setTtl(10);
         $response->setPublic();
-        $response->setTtl(25);
+        // replace this example code with whatever you need
+        return $response;
+    }
+
+    
+    public function esiAction(Request $request)
+    {
+        $response = $this->render('default/esi.html.twig');
+        // set the shared max age - which also marks the response as public
+        $response->setSharedMaxAge(5);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/pass", name="pass")
+     */
+    public function passedAction(Request $reques)
+    {
+        $response = new Response();
+        $response->setMaxAge(10);
+        $response->setPublic();
+        $response->setTtl(10);
         // replace this example code with whatever you need
         return $this->render(
             'default/index.html.twig', 
@@ -26,15 +56,5 @@ class DefaultController extends Controller
             ],
             $response
         );
-    }
-
-    
-    public function esiAction(Request $request)
-    {
-        $response = $this->render('default/esi.html.twig');
-        // set the shared max age - which also marks the response as public
-        $response->setSharedMaxAge(10);
-
-        return $response;
     }
 }
