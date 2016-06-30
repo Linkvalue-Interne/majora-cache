684864ccd76c87ba9660b8873accadc24f5696e7
Julien Bouffard <bouffard.jul@gmail.com>
2016-06-30 10:43:01 +0200

    add varnish test scenarios + add cache age in template

--- cachedemo/src/AppBundle/Controller/DefaultController.php
+++ cachedemo/src/AppBundle/Controller/DefaultController.php
@@ -11,49 +11,49 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
+        $age = 10;
         $response = $this->render(
             'default/index.html.twig',
             [
                 'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+                'age' => $age
             ]
         );
         $response->setETag(md5($response->getContent()));
         $response->isNotModified($request);
-        $response->setMaxAge(10);
-        $response->setTtl(10);
-        $response->setPublic();
+        $response->setMaxAge($age);
+        $response->setTtl($age);
+        // $response->setPublic();
         // replace this example code with whatever you need
         return $response;
     }
 
-
     public function esiAction(Request $request)
     {
-        $response = $this->render('default/esi.html.twig');
+        $age = 3;
+        $response = $this->render('default/esi.html.twig', ['age' => $age]);
 
         // set the shared max age - which also marks the response as public
-        $response->setSharedMaxAge(5);
-        $response->setPublic();
+        $response->setSharedMaxAge($age);
 
         return $response;
     }
 
-    /**
-     * @Route("/pass", name="pass")
-     */
-    public function passedAction(Request $reques)
+    public function passedAction(Request $request)
     {
         $response = new Response();
         $response->setMaxAge(10);
         $response->setPublic();
         $response->setTtl(10);
         // replace this example code with whatever you need
-        return $this->render(
+        $response = $this->render(
             'default/index.html.twig',
             [
                 'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-            ],
-            $response
+                'age' => 0
+            ]
         );
+
+        return $response;
     }
 }
