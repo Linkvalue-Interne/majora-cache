1a03784a63a8e191b2e63face08b6e397909ea33
Julien Bouffard <bouffard.jul@gmail.com>
2016-07-29 15:23:04 +0200

    add etag example + create basic test

--- cachedemo/src/AppBundle/Controller/DefaultController.php
+++ cachedemo/src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,9 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,7 +14,7 @@ class DefaultController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $age = 10;
+        $age = 15;
         $response = $this->render(
             'default/index.html.twig',
             [
@@ -19,18 +22,16 @@ class DefaultController extends Controller
                 'age' => $age
             ]
         );
-        $response->setETag(md5($response->getContent()));
-        $response->isNotModified($request);
-        $response->setMaxAge($age);
-        $response->setTtl($age);
-        // $response->setPublic();
-        // replace this example code with whatever you need
+        //$response->setETag(md5($response->getContent()));
+        // $response->isNotModified($request);
+        $response->setSharedMaxAge($age);
+
         return $response;
     }
 
     public function esiAction(Request $request)
     {
-        $age = 3;
+        $age = 5;
         $response = $this->render('default/esi.html.twig', ['age' => $age]);
 
         // set the shared max age - which also marks the response as public
@@ -39,21 +40,59 @@ class DefaultController extends Controller
         return $response;
     }
 
-    public function passedAction(Request $request)
+    public function etagAction(Request $request)
     {
-        $response = new Response();
-        $response->setMaxAge(10);
-        $response->setPublic();
-        $response->setTtl(10);
-        // replace this example code with whatever you need
+        $filename = sprintf(
+                '%s/data/etag.txt',
+                $this->getParameter('kernel.root_dir') 
+            );
+
+        $form = $this->createFormEtag();
+
+        $contentFile = file_get_contents($filename);
+
+        
         $response = $this->render(
-            'default/index.html.twig',
+            'default/etag.html.twig',
             [
-                'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-                'age' => 0
+                'content' => $contentFile,
+                'form' => $form->createView(),
             ]
         );
+        $response->setPublic(true);
+        $response->setETag(md5($response->getContent()));
 
         return $response;
+    }
+
+    public function contentCreateAction(Request $request)
+    {
+        $filename = sprintf(
+                '%s/data/etag.txt',
+                $this->getParameter('kernel.root_dir') 
+            );
+        $form = $this->createFormEtag();
+        
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $content = $form->get('content')->getData();
+            
+            $handle = fopen($filename, 'w+');
+            fwrite($handle, $content);
+            fclose($handle);
+        }
+        
+        return $this->redirect($this->generateUrl('app_etag'));
+    }
+
+    private function createFormEtag()
+    {
+        $form = $this->createFormBuilder()
+            ->setAction($this->generateUrl('app_content_create'))
+            ->add('content', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('save', SubmitType::class, ['label' => 'Save'])
+            ->getForm();
+
+        return $form;
     }
 }
